aee149ac71d4980e3960530a94499cdc38c7d7ba
Kumar Kisalaya <kumar.kisalaya@invisirisk.com>
2025-01-28 16:56:12 +0545

    Added log

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -65,6 +65,7 @@ echo "Iptables setup completed."
 
 echo "Download and RUN PSE"
 curl -o ~/pse https://ir-dev-public.s3.us-west-2.amazonaws.com/pse
+echo "Download cfg"
 curl -o ~/cfg.yaml https://ir-dev-public.s3.us-west-2.amazonaws.com/cfg.yaml
 curl -o ~/production/leaks.toml https://ir-dev-public.s3.us-west-2.amazonaws.com/leaks.toml
 curl -o ~/production/policy.json https://ir-dev-public.s3.us-west-2.amazonaws.com/policy.json
