72d26e6e637e615b9bb8155214df3917845b52d6
Kumar Kisalaya <kumar.kisalaya@invisirisk.com>
2025-01-30 10:21:26 +0545

    Added leaks option

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -69,7 +69,7 @@ echo "Starting proxy"
 export INVISIRISK_PORTAL=https://app.dev.invisirisk.com
 export INVISIRISK_JWT_TOKEN=1FL0OVuoSgeqy-y3T5cGGDjLGS4gaTRKtuTw4Jo9kwl8460Vz6lnnVvhrgh_FSTRmjYCsRgYEaxwBcuPP7Noyg
 sudo chmod +x ~/pse
-sudo -E ~/pse serve --policy ~/policy.json --config ~/cfg.yaml >> ~/pse.log 1>&1 &
+sudo -E ~/pse serve --policy ~/policy.json --config ~/cfg.yaml --config ~/leaks.toml >> ~/pse.log 1>&1 &
 PSE_PID=$!
 
 echo "Sleeping.."
